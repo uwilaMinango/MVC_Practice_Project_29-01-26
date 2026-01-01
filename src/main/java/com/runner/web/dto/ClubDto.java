707ccb681ac230de271aa1ed7ec7466f55739f7d
uwilaMinango <uwilaminango2604@yahoo.com>
2026-01-02 00:25:37 +0200

    First DTO creation

---
+++ src/main/java/com/runner/web/dto/ClubDto.java
@@ -0,0 +1,17 @@
+package com.runner.web.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ClubDto{
+    private int id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+}
