44d7e55159525cdc3980ee67cb92b42599dae8e8
uwilaMinango <uwilaminango2604@yahoo.com>
2026-01-01 23:14:36 +0200

    First model class created to represent a class in our DB

---
+++ src/main/java/com/runner/web/models/Club.java
@@ -0,0 +1,31 @@
+package com.runner.web.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+public class Club {
+    @Id
+    private int id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+}
